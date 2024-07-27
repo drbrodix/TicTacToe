66d09ed4ba906ab007dea818d94ab4e0dc788103
drbrodix <adamrigely@pm.me>
2024-07-27 13:40:07 +0200

    Initial Commit

---
+++ TicTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Adam Rigely on 27.07.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
